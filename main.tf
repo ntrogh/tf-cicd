e84ed6bb6a9efc95338cc8b31b59de87673823b3
ntrogh <ntrogh@hotmail.com>
2020-06-18 09:13:56 +0200

    add simple tf script

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Configure the AzureRM provider (using v2.1)
+provider "azurerm" {
+    version         = "~>2.1.0"
+    features {}
+}
+
+# Provision a resource group to hold all Azure resources
+resource "azurerm_resource_group" "main" {
+    name            = "tfcicd-resources"
+    location        = "westeurope"
+}
