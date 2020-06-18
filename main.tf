fa12be88e34ee6228fb8605aa6107daeacaab26b
ntrogh <ntrogh@hotmail.com>
2020-06-18 14:51:42 +0200

    Fix remote state

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Configure remote state on Azure Storage
 terraform {
     backend "azurerm" {
-        resource_group_name   = "tfcicd-tfstate"
-        storage_account_name  = "tfstate18062020"
+        resource_group_name   = "tstate"
+        storage_account_name  = "tstate18180"
         container_name        = "tstate"
         key                   = "terraform.tfstate"
     }
@@ -10,12 +10,16 @@ terraform {
 
 # Configure the AzureRM provider (using v2.1)
 provider "azurerm" {
-    version         = "~>2.14.0"
     features {}
 }
 
 # Provision a resource group to hold all Azure resources
-resource "azurerm_resource_group" "main" {
-    name            = "tfcicd-resources"
-    location        = "westeurope"
+resource "azurerm_resource_group" "state-demo-secure" {
+    name     = "state-demo"
+    location = "eastus"
+}
+
+resource "azurerm_resource_group" "state-demo-secure2" {
+    name     = "state-demo2"
+    location = "eastus"
 }
